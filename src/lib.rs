4c78bf40fd6ebe788ab4f02a71ebaf0142a9d936
David Reaver <me@davidreaver.com>
2023-04-28 20:10:20 -0700

    Print more multiboot info

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,60 @@ mod serial;
 mod vga_buffer;
 
 #[no_mangle]
-pub extern "C" fn rust_main(multiboot_info_ptr: u32) -> ! {
+pub extern "C" fn rust_main(multiboot_info_ptr: usize) -> ! {
     // ATTENTION: we have a somewhat small stack and no guard page
 
-    let boot_info = unsafe { load(multiboot_info_ptr as usize).unwrap() };
-    println!("Boot info: {:?}", boot_info);
+    let boot_info = unsafe { load(multiboot_info_ptr).unwrap() };
+
+    let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
+
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!(
+            "    start: 0x{:x}, end: 0x{:x}",
+            area.start_address(),
+            area.end_address()
+        );
+    }
+
+    let elf_sections_tag = boot_info
+        .elf_sections_tag()
+        .expect("Elf-sections tag required");
+
+    println!("kernel sections:");
+    for section in elf_sections_tag.sections() {
+        println!(
+            "    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+            section.start_address(),
+            section.size(),
+            section.flags()
+        );
+    }
+
+    let kernel_start = elf_sections_tag
+        .sections()
+        .map(|s| s.start_address())
+        .min()
+        .unwrap();
+    let kernel_end = elf_sections_tag
+        .sections()
+        .map(|s| s.end_address())
+        .max()
+        .unwrap();
+    println!(
+        "kernel start: 0x{:x}, end: 0x{:x}",
+        kernel_start, kernel_end
+    );
+
+    let multiboot_start = multiboot_info_ptr;
+    let multiboot_end = multiboot_start + boot_info.total_size();
+    println!(
+        "multiboot start: 0x{:x}, end: 0x{:x}",
+        multiboot_start, multiboot_end
+    );
 
     serial_println!("Testing serial port! {}", "hello");
 
-    println!("Hello World{}", "!");
-    println!("Hello again! Some numbers: {} {}", 42, (1.7 * 3.3) as u64);
-    println!("A float: {}", 1.337);
     panic!("Some panic message");
 
     loop {
