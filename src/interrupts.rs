a5f750ed507d2f78b43e25f537359aa228917098
David Reaver <me@davidreaver.com>
2023-04-29 11:52:24 -0700

    Make real page fault handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,5 +1,5 @@
 use lazy_static::lazy_static;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 use crate::{gdt, print, println};
 
@@ -7,7 +7,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        // idt.page_fault.set_handler_fn(page_fault_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
         unsafe {
             // set_stack_index is unsafe because the caller must ensure that the
@@ -60,12 +60,23 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
-// extern "x86-interrupt" fn page_fault_handler(
-//     stack_frame: InterruptStackFrame,
-//     _error_code: PageFaultErrorCode,
-// ) {
-//     println!("EXCEPTION: PAGE FAULT\n{:#?}", stack_frame);
-// }
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    println!("EXCEPTION: PAGE FAULT\n{:#?}", stack_frame);
+
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
 
 extern "x86-interrupt" fn general_protection_fault_handler(
     stack_frame: InterruptStackFrame,
