d29fbe2c2097c581c7faa377363fa1512fc47a7c
David Reaver <me@davidreaver.com>
2023-05-04 21:34:47 -0700

    Use mtools for FAT test image

--- crates/fat/scripts/create-test-image.sh
+++ crates/fat/scripts/create-test-image.sh
@@ -2,22 +2,18 @@
 
 set -eu
 
-name="fat12.img"
+rm -rf test-image
+mkdir -p test-image
+
+name="test-image/fat12.img"
 block_count=2000
 
 dd if=/dev/zero of="$name" bs=1024 "count=$block_count"
-mkfs.vfat -s 1 -F 12 -n "Test!" -i 12345678 "$name"
-mkdir -p mnt
-sudo mount -o loop "$name" mnt -o rw,uid="$USER",gid="$USER"
+mformat -i "$name"
 
 for i in $(seq 1 1000); do
-  echo "Rust is cool!" >>"mnt/long.txt"
+  echo "Rust is cool!" >> "test-image/long.txt"
 done
-
-echo "Rust is cool!" >>"mnt/short.txt"
-mkdir -p "mnt/very/long/path"
-echo "Rust is cool!" >>"mnt/very/long/path/test.txt"
-mkdir -p "mnt/very-long-dir-name"
-echo "Rust is cool!" >>"mnt/very-long-dir-name/very-long-file-name.txt"
-
-sudo umount mnt
+mcopy -i "$name" "test-image/long.txt" ::
+echo "Rust is cool!" >> "test-image/short.txt"
+mcopy -i "$name" "test-image/short.txt" ::
