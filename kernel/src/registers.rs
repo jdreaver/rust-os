adeebec067ceb07a2b5df6c38169bd40d8af1d5a
David Reaver <me@davidreaver.com>
2023-05-07 21:25:10 -0700

    Use different register types insteads of different methods

--- kernel/src/registers.rs
+++ kernel/src/registers.rs
@@ -1,5 +1,90 @@
-// Take inspiration from
+// Inspiration taken from
 // https://docs.rs/pci-driver/latest/pci_driver/#pci_struct-and-pci_bit_field
+//
+// TODO:
+// - Support for bit fields, like the PCI driver crate does.
+
+use core::marker::PhantomData;
+
+/// Register mapped to some underlying memory address.
+#[derive(Debug, Clone, Copy)]
+pub struct RegisterRW<T> {
+    ptr: *mut T,
+    _phantom: PhantomData<T>,
+}
+
+impl<T> RegisterRW<T> {
+    /// # Safety
+    ///
+    /// The caller must ensure that the address is a valid memory location for a
+    /// register of size `T`.
+    pub unsafe fn from_address(address: usize) -> Self {
+        Self {
+            ptr: address as *mut T,
+            _phantom: PhantomData,
+        }
+    }
+
+    pub fn read(&self) -> T {
+        unsafe { core::ptr::read_volatile(self.ptr) }
+    }
+
+    pub fn write(&self, val: T) {
+        unsafe {
+            core::ptr::write_volatile(self.ptr, val);
+        }
+    }
+}
+
+/// Read-only register mapped to some underlying memory address.
+#[derive(Debug, Clone, Copy)]
+pub struct RegisterRO<T> {
+    ptr: *const T,
+    _phantom: PhantomData<T>,
+}
+
+impl<T> RegisterRO<T> {
+    /// # Safety
+    ///
+    /// The caller must ensure that the address is a valid memory location for a
+    /// register of size `T`.
+    pub unsafe fn from_address(address: usize) -> Self {
+        Self {
+            ptr: address as *const T,
+            _phantom: PhantomData,
+        }
+    }
+
+    pub fn read(&self) -> T {
+        unsafe { core::ptr::read_volatile(self.ptr) }
+    }
+}
+
+/// Write-only register mapped to some underlying memory address.
+#[derive(Debug, Clone, Copy)]
+pub struct RegisterWO<T> {
+    ptr: *mut T,
+    _phantom: PhantomData<T>,
+}
+
+impl<T> RegisterWO<T> {
+    /// # Safety
+    ///
+    /// The caller must ensure that the address is a valid memory location for a
+    /// register of size `T`.
+    pub unsafe fn from_address(address: usize) -> Self {
+        Self {
+            ptr: address as *mut T,
+            _phantom: PhantomData,
+        }
+    }
+
+    pub fn write(&self, val: T) {
+        unsafe {
+            core::ptr::write_volatile(self.ptr, val);
+        }
+    }
+}
 
 #[macro_export]
 macro_rules! register_struct {
@@ -7,7 +92,7 @@ macro_rules! register_struct {
         $(#[$attr:meta])*
         $struct_name:ident {
             $(
-                $offset:literal => $name:ident : $type:ty
+                $offset:literal => $name:ident : $register_type:ident < $type:ty >
             ),* $(,)?
         }
     ) => {
@@ -23,7 +108,7 @@ macro_rules! register_struct {
             }
 
             $(
-                $crate::register_method_RW!($offset, $name, $type);
+                $crate::register_method!($offset, $name, $register_type, $type);
             )*
         }
     };
@@ -68,54 +153,46 @@ macro_rules! register_struct {
 // }
 
 #[macro_export]
-macro_rules! register_method_R {
-    ($offset:expr, $name:ident, $type:ty) => {
-        $crate::paste::paste! {
-            fn [<read_ $name>](&self) -> $type {
-                unsafe {
-                    let ptr = (self.address + $offset) as *const $type;
-                    core::ptr::read_volatile(ptr)
-                }
-            }
+macro_rules! register_method {
+    ($offset:expr, $name:ident, $register_type:ident, $type:ty) => {
+        fn $name(&self) -> $register_type<$type> {
+            unsafe { $register_type::from_address(self.address + $offset) }
         }
     };
 }
 
-#[macro_export]
-macro_rules! register_method_W {
-    ($offset:expr, $name:ident, $type:ty) => {
-        $crate::paste::paste! {
-            fn [<write_ $name>](&mut self, value: $type) {
-                unsafe {
-                    let ptr = (self.address + $offset) as *mut $type;
-                    core::ptr::write_volatile(ptr, value)
-                }
-            }
-        }
-    };
-}
+// #[macro_export]
+// macro_rules! register_method_R {
+//     ($offset:expr, $name:ident, $type:ty) => {
+//         $crate::paste::paste! {
+//             fn [<read_ $name>](&self) -> $type {
+//                 unsafe {
+//                     let ptr = (self.address + $offset) as *const $type;
+//                     core::ptr::read_volatile(ptr)
+//                 }
+//             }
+//         }
+//     };
+// }
 
-#[macro_export]
-macro_rules! register_method_RW {
-    ($offset:expr, $name:ident, $type:ty) => {
-        $crate::register_method_R!($offset, $name, $type);
-        $crate::register_method_W!($offset, $name, $type);
-    };
-}
+// #[macro_export]
+// macro_rules! register_method_W {
+//     ($offset:expr, $name:ident, $type:ty) => {
+//         $crate::paste::paste! {
+//             fn [<write_ $name>](&mut self, value: $type) {
+//                 unsafe {
+//                     let ptr = (self.address + $offset) as *mut $type;
+//                     core::ptr::write_volatile(ptr, value)
+//                 }
+//             }
+//         }
+//     };
+// }
 
-register_struct!(
-    MyStruct {
-        0x0 => hello: u8,
-        0x2 => blah: u16,
-    }
-);
-
-pub fn test_code() {
-    let mut my_struct = MyStruct::from_address(0x1000);
-    my_struct.write_hello(0x42);
-    my_struct.write_blah(0x1234);
-
-    let my_struct = MyStruct::from_address(0x1000);
-    assert_eq!(my_struct.read_hello(), 0x42);
-    assert_eq!(my_struct.read_blah(), 0x1234);
-}
+// #[macro_export]
+// macro_rules! register_method_RW {
+//     ($offset:expr, $name:ident, $type:ty) => {
+//         $crate::register_method_R!($offset, $name, $type);
+//         $crate::register_method_W!($offset, $name, $type);
+//     };
+// }
