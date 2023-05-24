aeb95da3f5b2c7ddab8bb32e8d715936c55f8b63
David Reaver <me@davidreaver.com>
2023-05-23 17:05:29 -0700

    Increase heap size

--- kernel/src/heap.rs
+++ kernel/src/heap.rs
@@ -13,7 +13,7 @@ use crate::memory;
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub(crate) const HEAP_START: usize = 0x_4444_4444_0000;
-pub(crate) const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+pub(crate) const HEAP_SIZE: usize = 10 * 1024 * 1024; // 10 MiB
 
 /// Maps pages for a kernel heap defined by `HEAP_START` and `HEAP_SIZE` and
 /// initializes `ALLOCATOR` with this heap.
