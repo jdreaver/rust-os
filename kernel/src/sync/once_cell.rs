59808e0b4550552255f2d1c60f099932cda7f829
David Reaver <me@davidreaver.com>
2023-06-11 11:46:57 -0700

    Use OnceCell under the hood in InitCell

--- kernel/src/sync/once_cell.rs
+++ kernel/src/sync/once_cell.rs
@@ -19,9 +19,9 @@ pub(super) struct OnceCell<T> {
 unsafe impl<T> Sync for OnceCell<T> where T: Send {}
 
 impl<T> OnceCell<T> {
-    pub(super) fn new() -> Self {
+    pub(super) const fn new() -> Self {
         Self {
-            message: UnsafeCell::new(MaybeUninit::zeroed()),
+            message: UnsafeCell::new(MaybeUninit::uninit()),
             ready: AtomicBool::new(false),
         }
     }
@@ -30,12 +30,11 @@ impl<T> OnceCell<T> {
     ///
     /// # Safety
     ///
-    /// This function should only be called once. This is important because
-    /// writing a value discards the old value, and we will never drop the old
-    /// value (this is a `MaybeUninit` feature/limitation). We panic if we call
-    /// this function twice, but it is still marked unsafe so the caller is
-    /// careful.
-    pub(super) unsafe fn send(&self, message: T) {
+    /// This function can only be called once. This is important because writing
+    /// a value discards the old value, and we will never drop the old value
+    /// (this is a `MaybeUninit` feature/limitation). We panic if we call this
+    /// function twice, but it is still marked unsafe so the caller is careful.
+    pub(super) unsafe fn set(&self, message: T) {
         unsafe {
             self.message.get().write(MaybeUninit::new(message));
         };
@@ -43,7 +42,16 @@ impl<T> OnceCell<T> {
         assert!(!old, "ERROR: Tried to write cell value twice");
     }
 
-    pub(super) fn receive(&self) -> Option<T> {
+    /// Extracts the value from the cell.
+    ///
+    /// # Safety
+    ///
+    /// This function can only be called once. It is itself "safe" because the
+    /// invariant on `set` ensures we only call `set` once, and the function
+    /// also uses the atomic bool `ready` to ensure we only read once. However,
+    /// the caller should ensure that if `get_once` is ever called, then other
+    /// functions that get references or copied to the value are _never_ called.
+    pub(super) unsafe fn get_once(&self) -> Option<T> {
         if self.ready.swap(false, Ordering::Acquire) {
             // Safety: We should only read a message once, since we are reifying
             // it from a single memory location. The swap above is an extra
@@ -54,7 +62,30 @@ impl<T> OnceCell<T> {
             None
         }
     }
+
+    /// Extracts a reference to the stored value.
+    pub(super) fn get_ref(&self) -> Option<&T> {
+        if self.ready.load(Ordering::Acquire) {
+            let ptr_ref = unsafe { self.message.get().as_ref().expect("null pointer") };
+            unsafe { Some(ptr_ref.assume_init_ref()) }
+        } else {
+            None
+        }
+    }
 }
+
+// impl<T: Copy> OnceCell<T> {
+//     /// Extracts a copy of the stored value.
+//     pub(super) unsafe fn get_copy(&self) -> Option<T> {
+//         if self.ready.load(Ordering::Acquire) {
+//             // Safety: We ensure that the type implements Copy, which is
+//             // required by `assume_init_read`.
+//             Some(self.message.get().read().assume_init_read())
+//         } else {
+//             None
+//         }
+//     }
+// }
 
 impl<T> Drop for OnceCell<T> {
     fn drop(&mut self) {
