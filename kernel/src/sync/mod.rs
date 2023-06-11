9c593dedf67a27cc1f8dc2314401ab66618109f3
David Reaver <me@davidreaver.com>
2023-06-11 11:21:50 -0700

    Properly snake_case sync file names

--- kernel/src/sync/mod.rs
+++ kernel/src/sync/mod.rs
@@ -1,12 +1,12 @@
-pub(crate) mod atomicint;
-pub(crate) mod initcell;
+pub(crate) mod atomic_int;
+pub(crate) mod init_cell;
 pub(crate) mod once_cell;
-pub(crate) mod oncechannel;
-pub(crate) mod spinlock;
-pub(crate) mod waitqueue;
+pub(crate) mod once_channel;
+pub(crate) mod spin_lock;
+pub(crate) mod wait_queue;
 
-pub(crate) use atomicint::*;
-pub(crate) use initcell::*;
-pub(crate) use oncechannel::*;
-pub(crate) use spinlock::*;
-pub(crate) use waitqueue::*;
+pub(crate) use atomic_int::*;
+pub(crate) use init_cell::*;
+pub(crate) use once_channel::*;
+pub(crate) use spin_lock::*;
+pub(crate) use wait_queue::*;
