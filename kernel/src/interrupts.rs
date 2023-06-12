1e5bd9348ef92a27d0c60eca34cf6ad55544c557
David Reaver <me@davidreaver.com>
2023-06-12 07:13:26 -0700

    Install an exception handler for every exception

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -80,18 +80,45 @@ lazy_static!(
     static ref IDT: InterruptDescriptorTable = {
         // TODO: Set up a handler for all of the possible CPU exceptions!
         let mut idt = InterruptDescriptorTable::new();
+        idt.divide_error.set_handler_fn(divide_error_handler);
+        idt.debug.set_handler_fn(debug_handler);
+        idt.non_maskable_interrupt
+            .set_handler_fn(non_maskable_interrupt_handler);
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.page_fault.set_handler_fn(page_fault_handler);
-        idt.general_protection_fault
-            .set_handler_fn(general_protection_fault_handler);
+        idt.overflow.set_handler_fn(overflow_handler);
+        idt.bound_range_exceeded
+            .set_handler_fn(bound_range_exceeded_handler);
+        idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
+        idt.device_not_available
+            .set_handler_fn(device_not_available_handler);
         unsafe {
             // set_stack_index is unsafe because the caller must ensure that the
             // used index is valid and not already used for another exception.
             idt.double_fault
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        };
+        idt.invalid_tss.set_handler_fn(invalid_tss_handler);
+        idt.segment_not_present
+            .set_handler_fn(segment_not_present_handler);
+        idt.stack_segment_fault
+            .set_handler_fn(stack_segment_fault_handler);
+        idt.general_protection_fault
+            .set_handler_fn(general_protection_fault_handler);
+        unsafe {
             idt.page_fault.set_handler_fn(page_fault_handler).set_stack_index(gdt::PAGE_FAULT_IST_INDEX);
         }
+        idt.x87_floating_point
+            .set_handler_fn(x87_floating_point_handler);
+        idt.alignment_check.set_handler_fn(alignment_check_handler);
+        idt.machine_check.set_handler_fn(machine_check_handler);
+        idt.simd_floating_point
+            .set_handler_fn(simd_floating_point_handler);
+        idt.virtualization.set_handler_fn(virtualization_handler);
+        idt.vmm_communication_exception
+            .set_handler_fn(vmm_communication_exception_handler);
+        idt.security_exception
+            .set_handler_fn(security_exception_handler);
 
         // Set up stub handlers for all external interrupts.
         seq!(N in 32..255 {
@@ -146,9 +173,80 @@ pub(crate) fn install_interrupt(interrupt_id: InterruptHandlerID, handler: Inter
     interrupt_index
 }
 
+extern "x86-interrupt" fn divide_error_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: DIVIDE ERROR\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn debug_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: DEBUG\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn non_maskable_interrupt_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: NON MASKABLE INTERRUPT\nStack Frame: {stack_frame:#?}");
+}
+
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     log::warn!("EXCEPTION: BREAKPOINT");
     log::warn!("{stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn overflow_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: OVERFLOW\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn bound_range_exceeded_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: BOUND RANGE EXCEEDED\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn invalid_opcode_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: INVALID_OPCODE\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn device_not_available_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: DEVICE NOT AVAILABLE\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn invalid_tss_handler(
+    stack_frame: InterruptStackFrame,
+    selector_index: u64,
+) {
+    panic!(
+        "EXCEPTION: INVALID TSS\nSelector Index: {selector_index}\nStack Frame: {stack_frame:#?}"
+    );
+}
+
+extern "x86-interrupt" fn segment_not_present_handler(
+    stack_frame: InterruptStackFrame,
+    selector_index: u64,
+) {
+    panic!(
+        "EXCEPTION: SEGMENT NOT PRESENT\nSelector Index: {selector_index}\nStack Frame: {stack_frame:#?}"
+    );
+}
+
+extern "x86-interrupt" fn stack_segment_fault_handler(
+    stack_frame: InterruptStackFrame,
+    selector_index: u64,
+) {
+    panic!(
+        "EXCEPTION: STACK_SEGMENT_FAULT\nSelector Index: {selector_index}\nStack Frame: {stack_frame:#?}"
+    );
+}
+
+extern "x86-interrupt" fn general_protection_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!(
+        "EXCEPTION: GENERAL PROTECTION FAULT\nError code: {error_code}\nStack Frame: {stack_frame:#?}"
+    );
 }
 
 extern "x86-interrupt" fn page_fault_handler(
@@ -165,24 +263,44 @@ extern "x86-interrupt" fn page_fault_handler(
     log::error!("Accessed Address (CR2): {:?}", accessed_address);
     log::error!("Error Code: {error_code:?}");
     log::error!("{stack_frame:#?}");
-
     loop {
         x86_64::instructions::hlt();
     }
 }
 
-extern "x86-interrupt" fn general_protection_fault_handler(
+extern "x86-interrupt" fn x87_floating_point_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: X87 FLOATING POINT\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn alignment_check_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) {
+    panic!("EXCEPTION: ALIGNMENT CHECK\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn machine_check_handler(stack_frame: InterruptStackFrame) -> ! {
+    panic!("EXCEPTION: MACHINE CHECK\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn simd_floating_point_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: SIMD FLOATING POINT\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn virtualization_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: VIRTUALIZATION\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn vmm_communication_exception_handler(
+    stack_frame: InterruptStackFrame,
+    vmexit_error_code: u64,
+) {
+    panic!("EXCEPTION: VMM COMMUNICATION\nVM exit error code: {vmexit_error_code}\nStack Frame: {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn security_exception_handler(
     stack_frame: InterruptStackFrame,
     error_code: u64,
 ) {
-    log::error!("EXCEPTION: GENERAL PROTECTION FAULT");
-    log::error!("error_code: {error_code}");
-    log::error!("{stack_frame:#?}");
-}
-
-extern "x86-interrupt" fn double_fault_handler(
-    stack_frame: InterruptStackFrame,
-    _error_code: u64,
-) -> ! {
-    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    panic!("EXCEPTION: SECURITY\nError code: {error_code}\nStack Frame: {stack_frame:#?}");
 }
